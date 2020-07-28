4b6da09628c7e63fb734db8d79a0654c73b43e4e
musiciancoder <andresguitarra1980@gmail.com>
2020-07-28 17:52:07 -0400

    202. Creando el controlador ChatController y el Mensaje

---
+++ src/main/java/info/ad80/springboot/backend/chat/controllers/ChatController.java
@@ -0,0 +1,21 @@
+package info.ad80.springboot.backend.chat.controllers;
+
+import java.util.Date;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+import info.ad80.springboot.backend.chat.models.documents.Mensaje;
+
+@Controller
+public class ChatController {
+	
+	@MessageMapping("/mensaje") // El cliente cada vez que escribe algo lo envía a /api/mensaje, porque configuramos el prefijo api en la clase WebSocketConfig
+	@SendTo("/chat/mensaje")  //Esta es la notificación a todos los clientes suscritos al evento de nombre "chat", que es el prefijo que dimos en clase WebSocketConf
+	private Mensaje recibeMensaje(Mensaje mensaje) { 
+		mensaje.setFecha(new Date().getTime());
+		mensaje.setTexto("Recibido por el broker: " + mensaje.getTexto());
+			return mensaje;
+	}
+}
